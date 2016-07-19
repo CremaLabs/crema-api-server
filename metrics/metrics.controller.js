990aa6260296df257638ed29073c927d4999c990
andrew brown <aebrownz11@gmail.com>
2016-07-19 15:59:00 -0700

    Added route and handler function to allow users to add coffee shop ratings Created handler function in metrics.controller Created route in metrics.routes Included the route in config/routes

---
+++ metrics/metrics.controller.js
@@ -0,0 +1,21 @@
+const Metric = require('./metrics.model');
+
+module.exports = {
+  addRating
+};
+
+
+// write a POSTed rating to the DB
+var addRating = function(req, res) {
+
+  Metric.create({
+    availRating: req.body.rating,
+    userMessage: req.body.message,
+    userID: req.body.userId,
+    shopID: req.body.shopId,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  .catch((err) => { console.log(err); });
+};
+
